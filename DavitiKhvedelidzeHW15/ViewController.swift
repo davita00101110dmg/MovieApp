02fb9dc753ac7575792b34f3a3c65af133deeaf2
davita00101110exe <daviti.khvedelidze.1@btu.edu.ge>
2022-07-12 11:24:18 +0400

    Initial Commit

---
+++ DavitiKhvedelidzeHW15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DavitiKhvedelidzeHW15
+//
+//  Created by Dato Khvedelidze on 12.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
